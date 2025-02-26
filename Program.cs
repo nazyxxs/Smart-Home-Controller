2c0a25f67e72437dd608e6ac13db437f58d3c7f0
nazyxxs <nazyagood15@gmail.com>
2025-02-26 19:33:30 +0200

    Update: Different files for clasess 1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+
+namespace HomeController
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHomeController home = SmartHomeController.Instance;
+
+            var light = new Light("Living Room Light");
+            var tv = new TV("Smart TV");
+
+            Sensor motionSensor = new Sensor();
+            Sensor nightModeSensor = new Sensor();
+
+            motionSensor.Attach(light);
+            nightModeSensor.Attach(tv);
+
+            // Збереження стану
+            var savedLightState = light.SaveState();
+            var savedTvState = tv.SaveState();
+
+            Console.WriteLine("\n--- Movement detected ---");
+            motionSensor.TriggerEvent("Movement detected!");
+
+            Console.WriteLine("\n--- Activating night mode ---");
+            nightModeSensor.TriggerEvent("Activating night mode!");
+
+            // Відновлення стану
+            Console.WriteLine("\n--- Restoring previous state ---");
+            light.RestoreState(savedLightState);
+            tv.RestoreState(savedTvState);
+        }
+    }
+}
+
---
+++ Sensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeController
+{
+    public class Sensor // Observer
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Attach(IObserver observer) => observers.Add(observer);
+        public void Detach(IObserver observer) => observers.Remove(observer);
+
+        public void TriggerEvent(string message)
+        {
+            Console.WriteLine($"[Sensor]: {message}");
+            foreach (var observer in observers)
+            {
+                observer.Update(message);
+            }
+        }
+    }
+}
